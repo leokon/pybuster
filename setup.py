725643fc62121ae1296990a8a026ddee1c025644
Leo Kontogiorgis <leokon95@gmail.com>
2020-01-31 14:22:33 +1100

    Add pip setup.py configuration for installation as a CLI tool

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='pybuster',
+    version='0.1.0',
+    description='A multithreaded brute-forcing tool for use with web URIs',
+    author='Leo Kontogiorgis',
+    author_email='leo@konto.dev',
+    url='https://github.com/leokon/pybuster',
+    packages=['pybuster'],
+    entry_points={
+        'console_scripts': ['pybuster=pybuster.pybuster:main']
+    }
+)
