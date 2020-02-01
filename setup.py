0f37aebbadcce5910242ea29c6045a927ba59a28
Leo Kontogiorgis <leokon95@gmail.com>
2020-02-01 12:18:26 +1100

    Add dependencies to setup.py for installation via pip

--- setup.py
+++ setup.py
@@ -2,12 +2,16 @@ from setuptools import setup
 
 setup(
     name='pybuster',
-    version='0.2.0',
+    version='0.2.1',
     description='A multithreaded brute-forcing tool for use with web URIs',
     author='Leo Kontogiorgis',
     author_email='leo@konto.dev',
     url='https://github.com/leokon/pybuster',
     packages=['pybuster'],
+    install_requires=[
+        'requests',
+        'tqdm'
+    ],
     entry_points={
         'console_scripts': ['pybuster=pybuster.pybuster:main']
     }
